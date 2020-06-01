d3c56c467b30e24dc5e4fd5d9f59b26ac2839bf7
Fabio Assoni Furtado <assonif@ibm.com>
2020-06-01 20:13:58 -0300

    Dashboard concluido

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,95 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src="../../assets/logo.svg" alt="Logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="/">
+          <img
+            src="https://avatars3.githubusercontent.com/u/47827967?s=460&u=9b75f5ad04632bcb6245975ec0442ab66b770c80&v=4"
+            alt="Fabio Furtado"
+          />
+          <div>
+            <strong>assonif/novo-teste</strong>
+            <p>Teste teste teste teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/">
+          <img
+            src="https://avatars3.githubusercontent.com/u/47827967?s=460&u=9b75f5ad04632bcb6245975ec0442ab66b770c80&v=4"
+            alt="Fabio Furtado"
+          />
+          <div>
+            <strong>assonif/novo-teste</strong>
+            <p>Teste teste teste teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/">
+          <img
+            src="https://avatars3.githubusercontent.com/u/47827967?s=460&u=9b75f5ad04632bcb6245975ec0442ab66b770c80&v=4"
+            alt="Fabio Furtado"
+          />
+          <div>
+            <strong>assonif/novo-teste</strong>
+            <p>Teste teste teste teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/">
+          <img
+            src="https://avatars3.githubusercontent.com/u/47827967?s=460&u=9b75f5ad04632bcb6245975ec0442ab66b770c80&v=4"
+            alt="Fabio Furtado"
+          />
+          <div>
+            <strong>assonif/novo-teste</strong>
+            <p>Teste teste teste teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/">
+          <img
+            src="https://avatars3.githubusercontent.com/u/47827967?s=460&u=9b75f5ad04632bcb6245975ec0442ab66b770c80&v=4"
+            alt="Fabio Furtado"
+          />
+          <div>
+            <strong>assonif/novo-teste</strong>
+            <p>Teste teste teste teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="/">
+          <img
+            src="https://avatars3.githubusercontent.com/u/47827967?s=460&u=9b75f5ad04632bcb6245975ec0442ab66b770c80&v=4"
+            alt="Fabio Furtado"
+          />
+          <div>
+            <strong>assonif/novo-teste</strong>
+            <p>Teste teste teste teste</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;

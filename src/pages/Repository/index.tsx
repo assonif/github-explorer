0c6c9d94e81af4bc29cdaa34ae044769b3b170b7
assonif <fabioassoni06@gmail.com>
2020-06-02 13:54:30 -0300

    Pagina Repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -9,10 +12,48 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <h1>
-      Repository:
-      {params.repository}
-    </h1>
+    <>
+      <Header>
+        <img src="../../assets/logo.svg" alt="Logo" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="kjhgkih" alt="logo" />
+          <div>
+            <strong>ojsdoidjsods</strong>
+            <p>doishodhsidhios</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>sdsdsds</strong>
+            <span>sasasa</span>
+          </li>
+          <li>
+            <strong>sdsdsds</strong>
+            <span>sasasa</span>
+          </li>
+          <li>
+            <strong>sdsdsds</strong>
+            <span>sasasa</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link key="wsdsdsd" to="dsdsdsd">
+          <div>
+            <strong>dsdsdsd</strong>
+            <p>dsfsdfsdfdsfsf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   );
 };
 
